5109bf57349e7d6d9a98df54c028722f1f94d316
Ren <tay.renfred@gmail.com>
2025-03-02 23:02:41 +1300

    Digital clock with circles

--- maeda_clock.js
+++ maeda_clock.js
@@ -37,6 +37,39 @@ function draw_clock(obj) {
   numberOne(300);
   numberOne(380);
   // 2
-  fill(255, 0, 0);
+  // fill(255, 0, 0);
+    // Top
   circle(500, 200, dot.size);
+  circle(517, 200, dot.size);
+  circle(534, 200, dot.size);
+  circle(551, 200, dot.size);
+  circle(568, 200, dot.size);
+  circle(584, 200, dot.size);
+    // top right
+  circle(584, 217, dot.size);
+  circle(584, 234, dot.size);
+  circle(584, 251, dot.size);
+  circle(584, 268, dot.size);
+  circle(584, 285, dot.size);
+    // middle
+  circle(568, 285, dot.size);
+  circle(551, 285, dot.size);
+  circle(534, 285, dot.size);
+  circle(517, 285, dot.size);
+  circle(500, 285, dot.size);
+    // bottom left
+  circle(500, 285, dot.size);
+  circle(500, 302, dot.size);
+  circle(500, 319, dot.size);
+  circle(500, 336, dot.size);
+    // bottom
+  circle(500, 353, dot.size);
+  circle(517, 353, dot.size);
+  circle(534, 353, dot.size);
+  circle(551, 353, dot.size);
+  circle(568, 353, dot.size);
+  circle(584, 353, dot.size);
+
+  // 1
+  numberOne(680);
 }
